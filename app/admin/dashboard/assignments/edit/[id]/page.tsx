7ee67fef2a1258dbf3daff2d621fe61a4b0161d8
Ewan <108713859+M1NDLE55@users.noreply.github.com>
2024-10-15 18:11:39 +0200

    fix: display modules on assignment edit page

--- app/admin/dashboard/assignments/edit/[id]/page.tsx
+++ app/admin/dashboard/assignments/edit/[id]/page.tsx
@@ -1,17 +1,17 @@
 import { AssignmentForm } from "@/components/assignments/assignmentForm";
 import { getAssignmentById } from "@/lib/actions/assignmentRequest";
-import { getLecturerModules } from "@/lib/actions/userRequests";
+import { getAllModules } from "@/lib/actions/modules/getAllModules";
 import { assignment } from "@/lib/definitions";
 
 export default async function Page({ params }: { params: { id: number } }) {
-  const modules = await getLecturerModules();
+  const response = await getAllModules();
   const assignment: assignment | null = await getAssignmentById(params.id);
 
   return (
     <main className="container mx-auto px-4">
       <h1 className="text-2xl font-bold mb-6">Edit Assignment</h1>
       <AssignmentForm
-        modules={modules}
+        modules={response.modules || []}
         assignment={assignment}
         isEditing={true}
       />
